ba4040645f9e48268628ff4fe695a97ca12ffa84
greenjon902 <greenjon902@gmail.com>
2022-06-12 12:43:23 +0100

    added information

--- src/main/java/com/greenjon902/gjms/GJMS.java
+++ src/main/java/com/greenjon902/gjms/GJMS.java
@@ -1,4 +1,8 @@
 package com.greenjon902.gjms;
 
 public class GJMS {
+    public static void main(String[] args) {
+        System.out.println("GJMS Starting...");
+        System.out.println("Version is 1.0-SNAPSHOT");
+    }
 }
