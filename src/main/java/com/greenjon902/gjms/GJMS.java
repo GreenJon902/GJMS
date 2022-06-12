74faf8fd1ee180923a14412b06f5c1f1a072b406
greenjon902 <greenjon902@gmail.com>
2022-06-12 12:56:06 +0100

    open socket

--- src/main/java/com/greenjon902/gjms/GJMS.java
+++ src/main/java/com/greenjon902/gjms/GJMS.java
@@ -1,8 +1,13 @@
 package com.greenjon902.gjms;
 
+import com.greenjon902.gjms.socket.SocketManager;
+
 public class GJMS {
     public static void main(String[] args) {
         System.out.println("GJMS Starting...");
         System.out.println("Version is 1.0-SNAPSHOT");
+
+        SocketManager socketManager = new SocketManager(25565);
+        socketManager.openSocket();
     }
 }
--- src/main/java/com/greenjon902/gjms/socket/SocketManager.java
+++ src/main/java/com/greenjon902/gjms/socket/SocketManager.java
@@ -1,4 +1,23 @@
 package com.greenjon902.gjms.socket;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class SocketManager {
+    private final int port;
+    private final ServerSocket serverSocket;
+
+    public SocketManager(int port) {
+        this.port = port;
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void openSocket() {
+
+    }
 }
