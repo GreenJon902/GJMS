32903c3cbb01a328031055e08b4575362711be13
greenjon902 <greenjon902@gmail.com>
2022-06-17 17:39:18 +0100

    added documentation for DataTypes.decodeFirstVarInt

--- src/main/java/com/greenjon902/gjms/socket/DataTypes.java
+++ src/main/java/com/greenjon902/gjms/socket/DataTypes.java
@@ -5,17 +5,26 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class DataTypes {
-    private static final int SEGMENT_BITS = 0x7F;
-    private static final int CONTINUE_BIT = 0x80;
+    private static final int SEGMENT_BITS = 0x7F; // 11111110
+    private static final int CONTINUE_BIT = 0x80; // 10000000
 
+    /**
+     * Reads the first variable length integer from the byte array.
+     * VarInts are stored in a way that each byte has 7 bits of data which is prefixed by a bit that shows whether
+     * to continue or not, if it is a 0 then the varInt is over, if it is a 1 then the varInt has another byte of data.
+     * VarInts can store up to 5 bits of data.
+     *
+     * @param bytes The byte array to read from
+     * @return The integer that was created
+     */
     public static int decodeFirstVarInt(byte[] bytes) {
-        int value = 0;
-        int position = 0;
+        int value = 0; // The number that is being decoded
+        int position = 0; // The position of value that is currently being edited
 
         for (byte currentByte : bytes) {
-            value |= (currentByte & SEGMENT_BITS) << position;
-            if ((currentByte & CONTINUE_BIT) == 0) break;
-            position += 7;
+            value |= (currentByte & SEGMENT_BITS) << position; // adds the new byte to the value
+            if ((currentByte & CONTINUE_BIT) == 0) break; // is the number finished?
+            position += 7; // because bytes in varInts store 7 bits of data
             if (position >= 32) throw new RuntimeException("VarInt is too big");
         }
 
