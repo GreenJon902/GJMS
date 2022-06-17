4078159757f6afb43424fcb42ab3736746ac6ddc
greenjon902 <greenjon902@gmail.com>
2022-06-17 17:39:41 +0100

    removed old initial packet handling stuff

--- src/main/java/com/greenjon902/gjms/socket/DataTypes.java
+++ src/main/java/com/greenjon902/gjms/socket/DataTypes.java
@@ -30,37 +30,4 @@ public class DataTypes {
 
         return value;
     }
-
-    public static int decodeVarInt(InputStream inputStream) throws IOException {
-        int value = 0;
-        int position = 0;
-        byte currentByte;
-
-        while (true) {
-            currentByte = (byte) inputStream.read();
-            value |= (currentByte & SEGMENT_BITS) << position;
-
-            if ((currentByte & CONTINUE_BIT) == 0) break;
-
-            position += 7;
-
-            if (position >= 32) throw new RuntimeException("VarInt is too big");
-        }
-
-        return value;
-    }
-
-    public static void writeVarInt(OutputStream outputStream, int value) throws IOException {
-        while (true) {
-            if ((value & ~SEGMENT_BITS) == 0) {
-                outputStream.write(value);
-                return;
-            }
-
-            outputStream.write((value & SEGMENT_BITS) | CONTINUE_BIT);
-
-            // Note: >>> means that the sign bit is shifted with the rest of the number rather than being left alone
-            value >>>= 7;
-        }
-    }
 }
