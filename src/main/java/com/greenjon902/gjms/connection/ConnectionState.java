fda4c57fc84562e2e6eb7b86cb3c526834776042
greenjon902 <greenjon902@gmail.com>
2022-06-19 17:43:54 +0100

    added doc for ConnectionState

--- src/main/java/com/greenjon902/gjms/connection/ConnectionState.java
+++ src/main/java/com/greenjon902/gjms/connection/ConnectionState.java
@@ -1,5 +1,8 @@
 package com.greenjon902.gjms.connection;
 
+/**
+ * The state that a {@link PlayerConnection} can currently be in.
+ */
 public enum ConnectionState {
     HANDSHAKE, STATUS, LOGIN, PLAY
 }
