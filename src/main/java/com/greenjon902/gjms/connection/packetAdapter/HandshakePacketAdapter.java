4c467f6cd4f87567e2bbf178cd7d3de81e35b6bb
greenjon902 <greenjon902@gmail.com>
2022-06-19 17:37:20 +0100

    added doc for PacketAdapter stuff

--- src/main/java/com/greenjon902/gjms/connection/packetAdapter/HandshakePacketAdapter.java
+++ src/main/java/com/greenjon902/gjms/connection/packetAdapter/HandshakePacketAdapter.java
@@ -1,5 +1,11 @@
 package com.greenjon902.gjms.connection.packetAdapter;
 
+import com.greenjon902.gjms.connection.ConnectionState;
+
+/**
+ * Contains the functionality for converting packets when the client's {@link ConnectionState} is
+ * {@link ConnectionState#HANDSHAKE}.
+ */
 public class HandshakePacketAdapter extends PacketAdapter {
 
 }
--- src/main/java/com/greenjon902/gjms/connection/packetAdapter/PacketAdapter.java
+++ src/main/java/com/greenjon902/gjms/connection/packetAdapter/PacketAdapter.java
@@ -1,5 +1,9 @@
 package com.greenjon902.gjms.connection.packetAdapter;
 
+/**
+ * Any class that extends this class will be used to decode bytes in a packet from a {@link java.io.InputStream} to a
+ * usable format, they can also encode a packet for sending through the network.
+ */
 public abstract class PacketAdapter {
 
 }
