296f7d3a8e6cdd169a94d152b7ae54e6fa400706
GreenJon902 <37347905+GreenJon902@users.noreply.github.com>
2022-08-03 14:00:48 +0200

    Update SetCompression.java

--- src/main/java/com/greenjon902/gjms/connection/prePlay/packetAdapter/login/packet/clientbound/SetCompression.java
+++ src/main/java/com/greenjon902/gjms/connection/prePlay/packetAdapter/login/packet/clientbound/SetCompression.java
@@ -1,1 +1,28 @@
+package com.greenjon902.gjms.connection.prePlay.packetAdapter.login.packet.clientbound;
 
+import com.greenjon902.gjms.connection.ClientboundPacket;
+
+public class SetCompression implements ClientboundPacket {
+    public final int threshold;
+
+    public SetCompression(int threshold) {
+        this.threshold = threshold;
+    }
+
+    /**
+     * The packet id is the way that minecraft clients are able to understand what each packet is and what it will do.
+     *
+     * @return The packet id of this packet
+     */
+    @Override
+    public int getPacketId() {
+        return 3;
+    }
+
+    @Override
+    public String toString() {
+        return "SetCompression{" +
+                "threshold=" + threshold +
+                '}';
+    }
+}
