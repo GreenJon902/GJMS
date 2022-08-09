0f137d1fb1da197ed9ac89160ca64cdeb825f089
GreenJon902 <37347905+GreenJon902@users.noreply.github.com>
2022-08-09 11:29:32 +0200

    added encryption response content

--- src/main/java/com/greenjon902/gjms/connection/prePlay/packetAdapter/login/packet/serverbound/EncryptionResponse.java
+++ src/main/java/com/greenjon902/gjms/connection/prePlay/packetAdapter/login/packet/serverbound/EncryptionResponse.java
@@ -1,1 +1,58 @@
+package com.greenjon902.gjms.connection.prePlay.packetAdapter.login.packet.servervound;
 
+import com.greenjon902.gjms.connection.ServerboundPacket;
+
+public class EncryptionResponse implements ServerboundPacket {
+    public final byte[] sharedSecret;
+    public final byte[] verifyToken;
+    public final long salt;
+    public final byte[] messageSignature;
+
+    public EncryptionResponse(byte[] sharedSecret, byte[] verifyToken) {
+        this(sharedSecret, verifyToken, null, null);
+    }
+
+    public EncryptionResponse(byte[] sharedSecret, long salt, byte[] messageSignature) {
+        this(sharedSecret, null, salt, messageSignature);
+    }
+
+    public EncryptionResponse(byte[] sharedSecret, byte[] verifyToken, long salt, byte[] messageSignature) {
+        this.sharedSecret = sharedSecret;
+        this.verifyToken = verifyToken;
+        this.salt = salt;
+        this.messageSignature = messageSignature;
+    }
+
+    public boolean hasVerifyToken() {
+        return !(verifyToken == null);
+    }
+
+    /**
+     * The packet id is the way that minecraft clients are able to understand what each packet is and what it will do.
+     *
+     * @return The packet id of this packet
+     */
+    @Override
+    public int getPacketId() {
+        return 1;
+    }
+
+    @Override
+    public String toString() {
+        bool hasVerifyToken = hasVerifyToken();
+        if (hasVerifyToken) {
+            return "EncryptionResponse{" +
+                    "sharedSecret=" + sharedSecret +
+                    "hasVerifyToken=" + hasVerifyToken +
+                    "verifyToken=" + verifyToken +
+                    '}';
+        } else {
+            return "EncryptionResponse{" +
+                    "sharedSecret=" + sharedSecret +
+                    "hasVerifyToken=" + hasVerifyToken +
+                    "salt=" + salt +
+                    "messageSignature=" + messageSignature +
+                    '}';
+        }
+    }
+}
