c9b42a1d1cd216ec5c5b5bf70c5a9710bc6781d4
greenjon902 <greenjon902@gmail.com>
2022-08-14 13:04:39 +0100

    added packet encoding for LoginPacketAdapter391to759

--- src/main/java/com/greenjon902/gjms/connection/prePlay/packetAdapter/login/LoginPacketAdapter391to759.java
+++ src/main/java/com/greenjon902/gjms/connection/prePlay/packetAdapter/login/LoginPacketAdapter391to759.java
@@ -5,6 +5,9 @@ import com.greenjon902.gjms.connection.Connection;
 import com.greenjon902.gjms.connection.ServerboundPacket;
 import com.greenjon902.gjms.connection.prePlay.PrePlayConnectionState;
 import com.greenjon902.gjms.connection.prePlay.packetAdapter.PrePlayPacketAdapter;
+import com.greenjon902.gjms.connection.prePlay.packetAdapter.login.packet.clientbound.EncryptionRequest;
+import com.greenjon902.gjms.connection.prePlay.packetAdapter.login.packet.clientbound.LoginSuccess;
+import com.greenjon902.gjms.connection.prePlay.packetAdapter.login.packet.clientbound.SetCompression;
 import com.greenjon902.gjms.connection.prePlay.packetAdapter.login.packet.serverbound.EncryptionResponse;
 import com.greenjon902.gjms.connection.prePlay.packetAdapter.login.packet.serverbound.LoginStart;
 import com.greenjon902.gjms.connection.prePlay.packetAdapter.status.packet.clientbound.PingResponse;
@@ -59,7 +62,35 @@ public class LoginPacketAdapter391to759 extends PrePlayPacketAdapter {
 
     @Override
     protected byte[] encodePacket(ClientboundPacket packet) {
-        byte[] content = null;
+        byte[] content;
+
+        if (packet instanceof EncryptionRequest encryptionRequest) {
+            byte[] serverId = encodeString(encryptionRequest.serverId);
+            byte[] publicKey = encodeBytes(encryptionRequest.publicKey);
+            byte[] verifyToken = encodeBytes(encryptionRequest.verifyToken);
+
+            content = new byte[serverId.length + publicKey.length + verifyToken.length];
+            System.arraycopy(serverId, 0, content, 0, serverId.length);
+            System.arraycopy(publicKey, 0, content, serverId.length, publicKey.length);
+            System.arraycopy(verifyToken, 0, content, serverId.length + publicKey.length,
+                    verifyToken.length);
+
+        } else if (packet instanceof LoginSuccess loginSuccess) {
+            byte[] uuid = encodeUUID(loginSuccess.uuid);
+            byte[] username = encodeString(loginSuccess.username);
+            byte[] properties = encodePropertyArray(loginSuccess.properties);
+
+            content = new byte[uuid.length + username.length + properties.length];
+            System.arraycopy(uuid, 0, content, 0, uuid.length);
+            System.arraycopy(username, 0, content, uuid.length, username.length);
+            System.arraycopy(properties, 0, content, uuid.length + username.length, properties.length);
+
+        } else if (packet instanceof SetCompression setCompression) {
+            byte[] threshold = encodeVarInt(setCompression.threshold);
+            content = threshold;
+        } else {
+            throw new RuntimeException("LoginPacketAdapter391to759 can only encode EncryptionRequest, LoginSuccess and SetCompression packets");
+        }
 
         byte[] packetId = encodeVarInt(packet.getPacketId());
         byte[] packetLength = encodeVarInt(packetId.length + content.length);
